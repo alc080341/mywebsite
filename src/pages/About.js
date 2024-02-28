eceeda3443855223112df7d4cc13e492e49c3394
alc080341 <chambersltony80@gmail.com>
2024-02-28 10:50:58 +0000

    1.0.0: change text on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,8 +12,9 @@ export default function About() {
         <div className="about-section">
           <div className="about-text-section">
             <h1>I have built many web based solutions</h1>
-            <p>I am a software developer with a number of years of experience in building web based solutions using a variety of modern tech.</p>
-            <p>I currently work in a commercial setting, with an agile team in a multinational company, where I have been involved in everything from user interface design to business logic and database table design. However, I have also built small independent projects, both for the benefit of small institutions, and for my own self improvement.</p>
+            <p>As a seasoned software developer, I offer a depth of experience in crafting innovative web-based solutions, harnessing a diverse array of cutting-edge technologies.</p>
+            <p>Throughout my journey as a software developer, I've thrived within dynamic commercial environments, collaborating seamlessly within agile teams at multinational corporations. Yet, I've also taken the reins on independent projects, tailoring solutions to the unique needs of small institutions while nurturing personal growth and expertise.</p>
+            <p>My contributions have spanned a wide spectrum of responsibilities, ranging from intricate user interface designs to the nuanced complexities of business logic and database architecture. Additionally, I've ventured into the realm of hybrid app development, crafting solutions for phones and tablets across both Android and iOS platforms.</p>
           </div>
           <div className="about-text-image">
             <div className="square square1">UX</div>
@@ -28,18 +29,37 @@ export default function About() {
             <div className="square square6">C#</div>
           </div>
           <div className="about-text-section">
-            <h3>Valuable Commercial Solutions Examples</h3>
-            <p>Integrated an automated XERO payment system, that eliminated the need for sending out invoices manually. Implemented the required code, and various network API calls, to create invoices automatically on customer request (from the current user interface of the product), or create new invoices when current ones expired.</p>
-            <p>Developed app versions of web-based software, for both Android and IOS, using the Apache Cordova environment, which exposed software that was just web-based, to a far larger customer base of app users.
-            Improved responsiveness of the software to cater for all screen sizes, further exposing the product to mobile and tablet users.</p>
+            <h3>Solutions to real-world business problems</h3>
+            <p>Streamlined Payment System Integration:</p>
+            <ul>
+              <li><p>Engineered an automated XERO payment system integration, effectively eliminating manual invoice processing.</p></li>
+              <li><p>Implemented requisite code and network API calls to facilitate automatic invoice generation upon customer request through the product's user interface.</p></li>
+              <li><p>Ensured seamless creation of new invoices to replace expired ones, enhancing operational efficiency.</p></li>
+            </ul>
+            <p>Mobile App Development and Optimization:</p>
+            <ul>
+              <li><p>Developed Android and iOS versions of web-based software using the Apache Cordova environment.</p></li>
+              <li><p>By leveraging Cordova, expanded the software's reach to a broader audience of mobile app users.</p></li>
+              <li><p>Enhanced software responsiveness across various screen sizes, optimizing user experience for mobile and tablet users.</p></li>
+            </ul>
           </div>
         </div>
         <div className="about-section">
           <div className="about-text-section">
-            <h3>Independent Solutions Examples</h3>
-            <p>Developed small questionnaire system for a team of Optometrists, which has helped them find 'red flag' patients faster, improving the efficiency of condition to treatment by putting patients on the correct referral pathway.</p>
-            <p>Developed a Wordpress website, along with custom plugins, which helped to promote the Headington Hill park, it's events and wildlife.</p>
-            <p>Developed a number of 'from scratch' custom websites to help promote a number of small businesses, including a portfolio site for a decorator, a window cleaner, and a life coach.</p>
+            <h3>Solutions for small businesses</h3>
+            <p>Optometrists' Questionnaire System:</p>
+            <ul>
+              <li><p>Created a streamlined questionnaire system tailored for a team of Optometrists.</p></li>
+              <li><p>Facilitated the identification of 'red flag' patients, expediting their referral to appropriate treatment pathways.</p></li>
+              <li><p>Significantly improved the efficiency of patient care by accelerating the transition from diagnosis to treatment.</p></li>
+            </ul>
+            <p>Bespoke portfolio and promotion websites</p>
+            <ul>
+              <li><p>Crafted a portfolio site for a decorator, a window cleaner, and a life coach, each tailored to their unique services and branding.</p></li>
+              <li><p>Enabled these businesses to establish a professional online presence, expanding their reach and enhancing market visibility.</p></li>
+            </ul>
+
+
           </div>
           <div className="about-text-image">
             <div className="square square7">Java</div>
@@ -54,9 +74,8 @@ export default function About() {
             <div className="square square12">Web</div>
           </div>
           <div className="about-text-section">
-            <h3>User-first design</h3>
-            <p>Software is nothing without the people who use it - I consider user experience best practices throughout the design process.</p>
-            <p>This includes designing for accessibility - considering the needs of potential users, and for a variety of different devices and screen sizes.</p>
+            <h3>My Approach</h3>
+            <p>Software derives its essence from the individuals who engage with it—I prioritize user experience best practices throughout the design process. </p><p>This commitment encompasses designing with accessibility in mind, acknowledging the diverse needs of potential users across various devices and screen dimensions.</p><p> Additionally, it entails maintaining active stakeholder engagement at every stage of development.</p>
             <p><a href='mailto:chambersltony80@gmail.com'>Contact me</a> for more information on the projects I have been involved in, and how I could help you with your project(s).</p>
           </div>
         </div>
